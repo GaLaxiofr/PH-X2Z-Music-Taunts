c5c3f5dd1eae339cbd76eea09d48248c2c3bab51
GaLaxio_fr <110476310+GaLaxiofr@users.noreply.github.com>
2022-09-02 22:15:38 +0200

    Initial Release

---
+++ For PropHunt X2Z/phx_music_taunts_pack/lua/autorun/server/sv_phx_taunts_pack.lua
@@ -0,0 +1,15 @@
+-- Let's say, if you have a directory under "sound/taunts_folder", then the last argument should only contains "taunts_folder".
+-- See this topic on how you add custom taunt within taunt scanner: 
+-- https://gmodgameservers.com/wiki/prophuntx/#5_Modifications/Add_Custom_Taunts+Add_Custom_Taunts
+
+-- Usage: list.Set("PHX.TauntScanFolder", "My Cool Taunt Pack", "taunts_folder")
+
+-- ABOVE THIS PARAPH, ALL LISTS SET FOR TAUNT SCANNER 
+
+-- DO NOT TOUCH, THIS IS THE FIRST TAUNT PACK !
+list.Set("PHX.TauntScanFolder", "Prop Hunt: Xtended Taunt Packs 1", "phx_new_taunts_pack") 
+
+-- THERE IS THE MUSIC TAUNT PACKS !
+list.Set("PHX.TauntScanFolder", "Prop Hunt: Music Taunt Pack", "phx_music_taunts_pack")
+
+-- WRITTEN BY GaLaxio. (PH:X2Z Support) (galaxio_fr STEAM_0:0:73626969)
