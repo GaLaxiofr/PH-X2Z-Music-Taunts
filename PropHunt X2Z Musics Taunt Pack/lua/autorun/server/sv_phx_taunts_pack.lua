4680a6de99b5eed75a2c14fd5446e1830c4e1699
GaLaxio_fr <110476310+GaLaxiofr@users.noreply.github.com>
2022-09-07 14:08:39 +0200

    v1.1.1
    Updated version, convert WAV. files into mp3. files

---
+++ PropHunt X2Z Musics Taunt Pack/lua/autorun/server/sv_phx_taunts_pack.lua
@@ -0,0 +1,7 @@
+-- Let's say, if you have a directory under "sound/taunts_folder", then the last argument should only contains "taunts_folder".
+-- See this topic on how you add custom taunt within taunt scanner: 
+-- https://gmodgameservers.com/wiki/prophuntx/#5_Modifications/Add_Custom_Taunts+Add_Custom_Taunts
+
+-- Usage: list.Set("PHX.TauntScanFolder", "My Cool Taunt Pack", "taunts_folder")
+-- Example:
+list.Set("PHX.TauntScanFolder", "Prop Hunt: Music Taunts", "phx_music_taunts_pack")
